98323d3947664b2babd8231d4481dc7eefb3c9f8
Ivan Martins <ivanmartins.ilha@gmail.com>
2022-07-06 13:40:35 -0300

    crio components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { PageOne, PageTwo, PageThree, PageFour } from "./PageOne";
+
 function App() {
-  return <h1>Hellloo</h1>;
+  return (
+    <>
+      <PageOne>
+        <div>
+          <img src="" alt="Seu logo" srcset="" />
+        </div>
+        <div>
+          <h2>
+            // Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Delectus // nostrum cupiditate alias perspiciatis architecto beatae
+            est, ab hic // voluptates dolorem animi vitae minus corporis. Sit
+            error praesentium // sapiente cumque tempore.
+          </h2>
+        </div>
+        <div>
+          <h3>
+            // Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quibusdam // sequi, magnam illo corrupti atque consectetur quod
+            incidunt id totam sit // magni modi ab asperiores repellat nihil
+            beatae repudiandae voluptates? // Tempora?
+          </h3>
+        </div>
+      </PageOne>
+      <hr />
+      <PageTwo>pagina 2</PageTwo>
+      <hr />
+      <PageThree>pagina 3</PageThree>;
+      <hr />
+      <PageFour>pagina 4</PageFour>
+    </>
+  );
 }
 
 export default App;
---
+++ src/PageOne.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const PageOne = styled.div`
+  background-color: rgba(49, 54, 172, 1);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+export const PageTwo = styled(PageOne)``;
+
+export const PageThree = styled(PageOne)``;
+
+export const PageFour = styled(PageOne)``;
+
+// render(
+//   <>
+//     <div>
+//       <img src="" alt="Seu logo" srcset="" />
+//     </div>
+//     <div>
+//       <h2>
+//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
+//         nostrum cupiditate alias perspiciatis architecto beatae est, ab hic
+//         voluptates dolorem animi vitae minus corporis. Sit error praesentium
+//         sapiente cumque tempore.
+//       </h2>
+//     </div>
+
+//     <div>
+//       <h3>
+//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
+//         sequi, magnam illo corrupti atque consectetur quod incidunt id totam sit
+//         magni modi ab asperiores repellat nihil beatae repudiandae voluptates?
+//         Tempora?
+//       </h3>
+//     </div>
+//   </>
+// );
